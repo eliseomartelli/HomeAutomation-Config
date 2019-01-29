11292e7ded6225065ad8ae446a1995a5ae47faad
Eliseo Martelli <martely98@gmail.com>
2019-01-29 14:44:52 +0100

    Update music_lights.py

--- appdaemon/apps/music_lights.py
+++ appdaemon/apps/music_lights.py
@@ -15,12 +15,13 @@ class MusicLights(hass.Hass):
  
   def initialize(self): 
     self.listen_state(self.change_led_color, self.args["media_player"], attribute = self.args["photo_attribute"])
+    self.listen_state(self.change_led_color, self.args["switch"])
 
   def change_led_color(self, entity, attribute, old, new, kwargs):
-    if (self.get_state(self.args["switch"]) == 'on'):
-      if new != old:
-        rgb_color = self.get_color(self.args["ha_url"] + new)
-        self.turn_on(self.args["light"], rgb_color=[rgb_color[0], rgb_color[1], rgb_color[2]])
+    if new != old:
+        if (self.get_state(self.args["switch"]) == 'on'):
+            rgb_color = self.get_color(self.args["ha_url"] + new)
+            self.turn_on(self.args["light"], rgb_color=[rgb_color[0], rgb_color[1], rgb_color[2]])
 
   def get_color(self, url):
     fd = urlopen(url)
