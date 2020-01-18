c9c0d8c5371d024f75c16b472387e720f791664a
Eliseo Martelli <me@eliseomartelli.it>
2020-01-18 22:35:07 +0100

    Started customizing lovelace

--- appdaemon/apps/music_lights.py
+++ appdaemon/apps/music_lights.py
@@ -19,11 +19,15 @@ class MusicLights(hass.Hass):
   def change_led_color(self, entity, attribute, old, new, kwargs):
     if (self.get_state(self.args["switch"]) == 'on'):
       if new != old:
-        rgb_color = self.get_color(self.args["ha_url"] + new)
+        print(attribute)
+        rgb = self.get_color(new)
+        rgb_color=rgb[0]
+        rgb_color1=rgb[1]
         self.turn_on(self.args["light"], rgb_color=[rgb_color[0], rgb_color[1], rgb_color[2]])
+        self.turn_on(self.args["light1"], rgb_color=[rgb_color1[0], rgb_color1[1], rgb_color1[2]])
 
   def get_color(self, url):
     fd = urlopen(url)
     f = io.BytesIO(fd.read())
     color_thief = ColorThief(f)
-    return color_thief.get_color(quality=2)
+    return color_thief.get_palette(color_count=2)
